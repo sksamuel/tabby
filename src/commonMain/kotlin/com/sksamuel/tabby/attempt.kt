9e26745b77e9a4c7a4e1f5cb46b1a899f6b5de01
sksamuel <sam@sksamuel.com>
2020-08-08 10:45:16 -0500

    Added flatten to attempt

--- src/commonMain/kotlin/com/sksamuel/tabby/attempt.kt
+++ src/commonMain/kotlin/com/sksamuel/tabby/attempt.kt
@@ -54,4 +54,6 @@ sealed class Attempt<out A> : Optional<A> {
    }
 }
 
+fun <T> Attempt<Attempt<T>>.flatten() = fold({ this }, { it })
+
 inline fun <A : B, B> Attempt<A>.getOrElse(f: (Err) -> B): B = fold({ f(it) }, { it })
