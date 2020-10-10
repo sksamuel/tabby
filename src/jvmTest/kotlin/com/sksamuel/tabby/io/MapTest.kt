41057a512ff358ff9b08aaa08dcc6121f596793d
sksamuel <sam@sksamuel.com>
2020-10-10 11:29:42 -0500

    added map test

--- src/jvmTest/kotlin/com/sksamuel/tabby/io/MapTest.kt
+++ src/jvmTest/kotlin/com/sksamuel/tabby/io/MapTest.kt
@@ -1,19 +1,13 @@
 package com.sksamuel.tabby.io
 
-import com.sksamuel.tabby.Either
 import com.sksamuel.tabby.right
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import io.kotest.matchers.types.shouldBeInstanceOf
 
 class MapTest : FunSpec() {
    init {
-      test("zip should invoke combinator on two successful inputs") {
-         IO.success("foo").zip(IO.success("bar")) { a, b -> a + b }.run() shouldBe "foobar".right()
-      }
-      test("zip should fail on left") {
-         IO.success("foo").zip(IO.effect<String> { error("bar") }) { a, b -> a + b }.run().shouldBeInstanceOf<Either.Left<*>>()
-         IO.effect<String> { error("bar") }.zip(IO.success("foo")) { a, b -> a + b }.run().shouldBeInstanceOf<Either.Left<*>>()
+      test("map should invoke combinator on success") {
+         IO.effect { "foo" }.map { it.length }.run() shouldBe 3.right()
       }
    }
 }
--- src/jvmTest/kotlin/com/sksamuel/tabby/io/ZipTest.kt
+++ src/jvmTest/kotlin/com/sksamuel/tabby/io/ZipTest.kt
@@ -11,7 +11,7 @@ class ZipTest : FunSpec() {
       test("zip should invoke combinator on two successful inputs") {
          IO.success("foo").zip(IO.success("bar")) { a, b -> a + b }.run() shouldBe "foobar".right()
       }
-      test("zip should fail on left") {
+      test("zip should fail if either io is a failure") {
          IO.success("foo").zip(IO.effect<String> { error("bar") }) { a, b -> a + b }.run().shouldBeInstanceOf<Either.Left<*>>()
          IO.effect<String> { error("bar") }.zip(IO.success("foo")) { a, b -> a + b }.run().shouldBeInstanceOf<Either.Left<*>>()
       }
