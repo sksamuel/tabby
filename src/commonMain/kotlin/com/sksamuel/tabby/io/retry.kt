7de41f79d9d359244462a3682ed40232c823cf1c
sksamuel <sam@sksamuel.com>
2020-10-10 11:33:57 -0500

    Added retry tests

---
+++ src/commonMain/kotlin/com/sksamuel/tabby/io/retry.kt
@@ -0,0 +1,26 @@
+package com.sksamuel.tabby.io
+
+import com.sksamuel.tabby.Either
+import com.sksamuel.tabby.left
+import com.sksamuel.tabby.right
+import kotlinx.coroutines.delay
+
+fun <E, T> IO<E, T>.retry(attempts: Int, interval: Long): IO<E, T> {
+   require(attempts > 0)
+   return object : IO<E, T>() {
+      override suspend fun apply(): Either<E, T> = attempt(attempts - 1)
+      private suspend fun attempt(remaining: Int): Either<E, T> {
+         return this@retry.apply().fold(
+            {
+               if (remaining > 0) {
+                  delay(interval)
+                  attempt(remaining - 1)
+               } else {
+                  it.left()
+               }
+            },
+            { it.right() }
+         )
+      }
+   }
+}
