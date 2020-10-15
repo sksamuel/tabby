001909ff2c55f22049ea233814b02ea1b92df29e
sksamuel <sam@sksamuel.com>
2020-10-14 20:18:24 -0500

    Added test for effect.either

---
+++ src/jvmTest/kotlin/com/sksamuel/tabby/io/EitherTest.kt
@@ -0,0 +1,19 @@
+package com.sksamuel.tabby.io
+
+import com.sksamuel.tabby.left
+import com.sksamuel.tabby.right
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class EitherTest : FunSpec() {
+   init {
+      test("Either.effect should wrap a right in an IO.success") {
+         "hello".right().effect().flatMap { IO.success { it.length } }.run().getRightUnsafe() shouldBe 5
+         "hello".right().effect().flatMap { IO.success("foo") }.run().getRightUnsafe() shouldBe "foo"
+      }
+
+      test("Either.effect should wrap a left in an IO.failure") {
+         "boom".left().effect().flatMap { IO.success { "foo" } }.run().getLeftUnsafe() shouldBe "boom"
+      }
+   }
+}
