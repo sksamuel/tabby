7ec2a6cd33e6a5386ed5d6c8111869d12a89b078
sksamuel <sam@sksamuel.com>
2020-10-14 20:20:12 -0500

    updated flat map tests

--- src/jvmTest/kotlin/com/sksamuel/tabby/io/FlatMapTest.kt
+++ src/jvmTest/kotlin/com/sksamuel/tabby/io/FlatMapTest.kt
@@ -1,5 +1,6 @@
 package com.sksamuel.tabby.io
 
+import com.sksamuel.tabby.left
 import com.sksamuel.tabby.right
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
@@ -8,6 +9,8 @@ class FlatMapTest : FunSpec() {
    init {
       test("flatmap should invoke combinator on success") {
          IO.effect { "foo" }.flatMap { IO.effect { it.length } }.run() shouldBe 3.right()
+         IO.effect { "foo" }.flatMap { IO.success("foo") }.run() shouldBe "foo".right()
+         IO.effect { "foo" }.flatMap { IO.failure("boom") }.run() shouldBe "boom".left()
       }
       test("flatmap should retain error") {
          IO.effect { error("boom") }.flatMap { IO.effect { "hello" } }.run().getLeftUnsafe().message shouldBe "boom"
