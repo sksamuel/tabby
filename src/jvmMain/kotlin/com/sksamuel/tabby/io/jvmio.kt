e6541411e4778199737c4502362f80d0e5f7c3da
sksamuel <sam@sksamuel.com>
2020-11-09 17:10:14 -0600

    Tweaked the way use works

--- src/jvmMain/kotlin/com/sksamuel/tabby/io/jvmio.kt
+++ src/jvmMain/kotlin/com/sksamuel/tabby/io/jvmio.kt
@@ -10,8 +10,10 @@ package com.sksamuel.tabby.io
  * The resource is guaranteed to be closed before the effect completes.
  * Any error in acquisition is ignored.
  */
-fun <A : AutoCloseable, B> IO.Companion.useWith(acquire: suspend () -> A,
-                                                f: (A) -> Task<B>): Task<B> {
+fun <A : AutoCloseable, B> IO.Companion.useWith(
+   acquire: suspend () -> A,
+   f: (A) -> Task<B>,
+): Task<B> {
    return effect(acquire).flatMap { a ->
       val close = effect { a.close() }
       f(a).tap { close }.tapError { close }
@@ -20,17 +22,19 @@ fun <A : AutoCloseable, B> IO.Companion.useWith(acquire: suspend () -> A,
 
 /**
  * Returns an effect that acquires an [AutoCloseable] resource using the given
- * [acquire] function, and then applies that resource to the given [f] function.
+ * [acquire] effect, and then applies that resource to the given [apply] effect.
  *
  * Both the acquire and action functions can be effectful, and will be handled as such.
  *
  * The resource is guaranteed to be closed before the effect completes.
  * Any error in acquisition is ignored.
  */
-fun <A : AutoCloseable, B> IO.Companion.use(acquire: suspend () -> A,
-                                            f: suspend (A) -> B): Task<B> {
-   return effect(acquire).flatMap { a ->
+fun <A : AutoCloseable, B> IO.Companion.use(
+   acquire: Task<A>,
+   apply: (A) -> Task<B>,
+): Task<B> {
+   return acquire.flatMap { a ->
       val close = effect { a.close() }
-      effect { f(a) }.tap { close }.tapError { close }
+      apply(a).tap { close }.tapError { close }
    }
 }
