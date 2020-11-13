b9b5578db04bea2791f849c07dfe7d7ef99a0db3
sksamuel <sam@sksamuel.com>
2020-11-12 22:59:22 -0600

    Updated List<A>.mapNotEmpty

--- src/commonMain/kotlin/com/sksamuel/tabby/option.kt
+++ src/commonMain/kotlin/com/sksamuel/tabby/option.kt
@@ -204,8 +204,8 @@ fun <A> List<A>.firstOrNone(): Option<A> = this.firstOrNull().toOption()
 
 fun <A> List<Option<A>>.filterNotEmpty(): List<A> = this.mapNotNull { it.orNull() }
 
-fun <A, B> List<Option<A>>.mapNotEmpty(f: (A) -> Option<B>): List<B> =
-   this.filterNotEmpty().mapNotNull { f(it).orNull() }
+fun <A, B> List<A>.mapNotEmpty(f: (A) -> Option<B>): List<B> =
+   this.mapNotNull { f(it).orNull() }
 
 @Deprecated("Use flatMap with Optional")
 inline fun <T, U : Any> List<T>.flatMapOption(f: (T) -> Option<U>): List<U> = mapNotNull { f(it).orNull() }
