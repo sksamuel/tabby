49274a17ac898527a6073816ba788a873ced3296
sksamuel <sam@sksamuel.com>
2020-11-30 17:05:38 -0600

    Added Either.mapN for arity 5 and 6

--- src/commonMain/kotlin/com/sksamuel/tabby/either/mapn.kt
+++ src/commonMain/kotlin/com/sksamuel/tabby/either/mapn.kt
@@ -36,19 +36,43 @@ inline fun <A, B, C, D, ERROR, R> Either.Companion.mapN(
    return fn(a.getRightUnsafe(), b.getRightUnsafe(), c.getRightUnsafe(), d.getRightUnsafe()).right()
 }
 
-
 inline fun <A, B, C, D, E, ERROR, R> Either.Companion.mapN(
    a: Either<ERROR, A>,
    b: Either<ERROR, B>,
    c: Either<ERROR, C>,
    d: Either<ERROR, D>,
-   e: Either<ERROR, D>,
-   fn: (A, B, C, D) -> R,
+   e: Either<ERROR, E>,
+   fn: (A, B, C, D, E) -> R,
 ): Either<ERROR, R> {
    if (a.isLeft) return a as Either<ERROR, R>
    if (b.isLeft) return b as Either<ERROR, R>
    if (c.isLeft) return c as Either<ERROR, R>
    if (d.isLeft) return d as Either<ERROR, R>
-   if (e.isLeft) return d as Either<ERROR, R>
-   return fn(a.getRightUnsafe(), b.getRightUnsafe(), c.getRightUnsafe(), d.getRightUnsafe()).right()
+   if (e.isLeft) return e as Either<ERROR, R>
+   return fn(a.getRightUnsafe(), b.getRightUnsafe(), c.getRightUnsafe(), d.getRightUnsafe(), e.getRightUnsafe()).right()
+}
+
+inline fun <A, B, C, D, E, F, ERROR, R> Either.Companion.mapN(
+   a: Either<ERROR, A>,
+   b: Either<ERROR, B>,
+   c: Either<ERROR, C>,
+   d: Either<ERROR, D>,
+   e: Either<ERROR, E>,
+   f: Either<ERROR, F>,
+   fn: (A, B, C, D, E, F) -> R,
+): Either<ERROR, R> {
+   if (a.isLeft) return a as Either<ERROR, R>
+   if (b.isLeft) return b as Either<ERROR, R>
+   if (c.isLeft) return c as Either<ERROR, R>
+   if (d.isLeft) return d as Either<ERROR, R>
+   if (e.isLeft) return e as Either<ERROR, R>
+   if (f.isLeft) return e as Either<ERROR, R>
+   return fn(
+      a.getRightUnsafe(),
+      b.getRightUnsafe(),
+      c.getRightUnsafe(),
+      d.getRightUnsafe(),
+      e.getRightUnsafe(),
+      f.getRightUnsafe()
+   ).right()
 }
