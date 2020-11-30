37a809cb68f872176d660c7c2cd26c98814dc777
sksamuel <sam@sksamuel.com>
2020-11-29 21:05:50 -0600

    Added either flatMap that accepts options

--- src/commonMain/kotlin/com/sksamuel/tabby/either/options.kt
+++ src/commonMain/kotlin/com/sksamuel/tabby/either/options.kt
@@ -1,12 +1,21 @@
 package com.sksamuel.tabby.either
 
 import com.sksamuel.tabby.option.Option
+import kotlin.experimental.ExperimentalTypeInference
 
 fun <A, B> Either<A, Option<B>>.flatten(f: () -> A): Either<A, B> = when (this) {
    is Either.Left -> this
    is Either.Right -> b.fold({ f().left() }, { it.right() })
 }
 
-fun <A, B> Either<A, Option<B>>.trifold(fa: (A) -> Unit, fn: (B) -> Unit, fs: (B) -> Unit) {
+fun <A, B> Either<A, Option<B>>.trifold(fa: (A) -> Unit, fn: () -> Unit, fs: (B) -> Unit) {
    this.fold(fa, { it.fold(fn, fs) })
+}
+
+@OptIn(ExperimentalTypeInference::class)
+@OverloadResolutionByLambdaReturnType
+fun <B, C> Either<Throwable, B>.flatMap(f: (B) -> Option<C>): Either<Throwable, C> {
+   return this@flatMap.flatMap { b ->
+      f(b).fold({ NoSuchElementException().left() }, { it.right() })
+   }
 }
