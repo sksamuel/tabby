1498e4432f4e70d7c674439926fca1df4c99ed8f
sksamuel <sam@sksamuel.com>
2020-12-01 12:17:19 -0600

    Added deprecated type aliases

---
+++ src/commonMain/kotlin/com/sksamuel/tabby/alias.kt
@@ -0,0 +1,7 @@
+package com.sksamuel.tabby
+
+@Deprecated("use com.sksamuel.tabby.either.Either")
+typealias Either<A, B> = com.sksamuel.tabby.either.Either<A, B>
+
+@Deprecated("use com.sksamuel.tabby.option.Option")
+typealias Option<A> = com.sksamuel.tabby.option.Option<A>
