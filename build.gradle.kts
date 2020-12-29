b2ba68e66a7f95146dabaedd42ec45bca0b59f43
sksamuel <sam@sksamuel.com>
2020-12-29 15:34:17 -0600

    Extracted publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,4 +59,4 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
    kotlinOptions.jvmTarget = "1.8"
 }
 
-apply(from = "../publish.gradle.kts")
+apply(from = "./publish.gradle.kts")
--- tabby-jackson/build.gradle.kts
+++ tabby-jackson/build.gradle.kts
@@ -86,4 +86,4 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach 
    kotlinOptions.jvmTarget = "1.8"
 }
 
-apply(from = "../../publish.gradle.kts")
+apply(from = "../publish.gradle.kts")
