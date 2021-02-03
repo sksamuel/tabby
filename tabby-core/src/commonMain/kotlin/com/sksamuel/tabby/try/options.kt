49a26cb414afdb8ccda3ea5bdf0a773d5fbc7d8c
sksamuel <sam@sksamuel.com>
2021-02-03 12:49:05 -0600

    Added options.toTry

--- tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/try/options.kt
+++ tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/try/options.kt
@@ -2,6 +2,10 @@ package com.sksamuel.tabby.`try`
 
 import com.sksamuel.tabby.option.Option
 
+fun <A> Option<A>.toTry(message: String) = fold({ message.error() }, { it.value() })
+
+fun <A> Option<A>.toTry(ifEmpty: () -> Throwable) = fold({ ifEmpty().error() }, { it.value() })
+
 fun <A> Try<Option<A>>.flatten(ifEmpty: () -> A): Try<A> = when (this) {
    is Try.Failure -> this
    is Try.Success -> value.fold({ ifEmpty().value() }, { it.value() })
