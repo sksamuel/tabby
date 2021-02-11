046674c02de6204ce4b3accb9e12d3e99cada0bb
sksamuel <sam@sksamuel.com>
2021-02-11 13:58:10 -0600

    Added overloaded timeout

--- tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/effects/timeout.kt
+++ tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/effects/timeout.kt
@@ -1,15 +1,39 @@
 package com.sksamuel.tabby.effects
 
 import com.sksamuel.tabby.`try`.Try
+import com.sksamuel.tabby.`try`.failure
 import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.withTimeout
+import kotlin.time.Duration
+
+/**
+ * Returns an effect that will decorate this effect with a timeout,
+ * setting the result to a [TimeoutCancellationException]
+ * in the case of a timeout.
+ */
+fun <A> IO<A>.timeout(duration: Duration): IO<A> = timeout(duration.toLongMilliseconds())
 
 fun <A> IO<A>.timeout(millis: Long): IO<A> = object : IO<A>() {
    override suspend fun apply(): Try<A> {
       return try {
          withTimeout(millis) { this@timeout.apply() }
       } catch (e: TimeoutCancellationException) {
-         Try.failure(e)
+         e.failure()
+      }
+   }
+}
+
+
+/**
+ * Returns an effect that will decorate this effect with a timeout,
+ * setting the result to the given error in the case of a timeout.
+ */
+fun <A> IO<A>.timeout(duration: Duration, f: () -> Throwable): IO<A> = object : IO<A>() {
+   override suspend fun apply(): Try<A> {
+      return try {
+         withTimeout(duration.toLongMilliseconds()) { this@timeout.apply() }
+      } catch (e: TimeoutCancellationException) {
+         f().failure()
       }
    }
 }
