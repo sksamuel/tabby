6689b3b8e47689291cb98b9420587479f517bf3d
sksamuel <sam@sksamuel.com>
2021-02-22 11:45:50 -0600

    Added option/try transpose

--- tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/try/options.kt
+++ tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/try/options.kt
@@ -1,10 +1,10 @@
 package com.sksamuel.tabby.`try`
 
 import com.sksamuel.tabby.option.Option
+import com.sksamuel.tabby.option.none
+import com.sksamuel.tabby.option.some
 
 fun <A> Option<A>.toTry(message: String) = fold({ message.error() }, { it.value() })
-
-fun <A> Option<A>.toTry(ifEmpty: () -> Throwable) = fold({ ifEmpty().error() }, { it.value() })
 
 fun <A> Try<Option<A>>.flatten(ifEmpty: () -> A): Try<A> = when (this) {
    is Try.Failure -> this
@@ -18,3 +18,12 @@ fun <A> Try<Option<A>>.flatten(emptyMessage: String? = null): Try<A> = when (thi
       is Option.None -> Try.Failure(NoSuchElementException(emptyMessage))
    }
 }
+
+/**
+ * Acts as a monad transformer, converting an OptionTry into a TryOption.
+ */
+fun <A> Option<Try<A>>.transpose(): Try<Option<A>> =
+   fold({ none.success() }, { t -> t.fold({ it.failure() }, { it.some().success() }) })
+
+fun <A> Try<Option<A>>.transpose(): Option<Try<A>> =
+   fold({ it.failure().some() }, { o -> o.fold({ none }, { it.success().some() }) })
