01519a31e56e8f93e41a11d9076ff59ff0557147
sksamuel <sam@sksamuel.com>
2021-03-17 10:58:26 -0500

    Added Eq overloads to collections for any / none

--- tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/eq/eq.kt
+++ tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/eq/eq.kt
@@ -7,3 +7,6 @@ fun interface Eq<A> {
    fun eq(a1: A, a2: A): Boolean
    fun neq(a1: A, a2: A): Boolean = !eq(a1, a2)
 }
+
+fun <A> List<A>.any(a: A, eq: Eq<A>): Boolean = this.any { eq.eq(it, a) }
+fun <A> List<A>.none(a: A, eq: Eq<A>): Boolean = this.none { eq.eq(it, a) }
