cf11898381869076e0efc2440c38bd53315f4667
sksamuel <sam@sksamuel.com>
2021-03-30 20:22:38 -0500

    Made nullable fold inline

--- tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/nulls/fold.kt
+++ tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/nulls/fold.kt
@@ -1,5 +1,5 @@
 package com.sksamuel.tabby.nulls
 
-fun <A, B> A?.fold(ifNull: () -> B, ifNotNull: (A) -> B): B {
+inline fun <A, B> A?.fold(ifNull: () -> B, ifNotNull: (A) -> B): B {
    return if (this == null) ifNull() else ifNotNull(this)
 }
