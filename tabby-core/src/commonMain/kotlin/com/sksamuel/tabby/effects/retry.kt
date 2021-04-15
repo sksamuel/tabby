963db75317b35f5c98cf427eccfe9d91fdc11533
sksamuel <sam@sksamuel.com>
2021-04-15 16:15:44 -0500

    Updated names of retry

--- tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/effects/retry.kt
+++ tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/effects/retry.kt
@@ -4,13 +4,15 @@ import com.sksamuel.tabby.`try`.Try
 import com.sksamuel.tabby.`try`.catch
 
 /**
+ * Repeats the given effect, according to the specified schedule, until it completes normally.
+ */
+suspend fun Schedule.retryUnit(f: suspend () -> Unit) {
+   repeatWhile(this, { catch { f() } }) { it.isFailure }
+}
+
+/**
  * Repeats the given effect, according to the specified schedule, while the result is a failure.
  */
 suspend fun <A> Schedule.retry(f: suspend () -> Try<A>): Try<A> {
    return repeatWhile(this, { f() }) { it.isFailure }
 }
-
-/**
- * Repeats the given effect, according to the specified schedule, while it fails to complete normally.
- */
-suspend fun <A> Schedule.retrySafe(f: suspend () -> A): Try<A> = retry { catch { f() } }
