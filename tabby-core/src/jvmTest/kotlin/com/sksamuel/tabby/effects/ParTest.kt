cf563e5d29f09706ae72140f85426b7868ef1c9b
sksamuel <sam@sksamuel.com>
2021-05-07 13:18:53 -0500

    Added tests for IO.par with effect typelias

--- tabby-core/src/jvmTest/kotlin/com/sksamuel/tabby/effects/ParTest.kt
+++ tabby-core/src/jvmTest/kotlin/com/sksamuel/tabby/effects/ParTest.kt
@@ -1,5 +1,8 @@
 package com.sksamuel.tabby.effects
 
+import com.sksamuel.tabby.Tuple3
+import com.sksamuel.tabby.Tuple4
+import com.sksamuel.tabby.`try`.success
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.Dispatchers
@@ -8,6 +11,102 @@ import kotlin.time.milliseconds
 
 class ParTest : FunSpec() {
    init {
+
+      test("parN(2) should run in parallel").config(timeout = 350.milliseconds) {
+         val a: Effect<String> = {
+            delay(250)
+            "foo".success()
+         }
+         val b: Effect<String> = {
+            delay(250)
+            "bar".success()
+         }
+         IO.parN(a, b).getValueUnsafe() shouldBe Pair("foo", "bar")
+      }
+
+      test("parN(2) should short circuit").config(timeout = 500.milliseconds) {
+         val a: Effect<String> = {
+            delay(100)
+            error("boom")
+         }
+         val b: Effect<String> = {
+            delay(1000)
+            "bar".success()
+         }
+         IO.parN(a, b).getErrorUnsafe().message shouldBe "boom"
+      }
+
+      test("parN(3) should run in parallel").config(timeout = 350.milliseconds) {
+         val a: Effect<String> = {
+            delay(250)
+            "foo".success()
+         }
+         val b: Effect<String> = {
+            delay(250)
+            "bar".success()
+         }
+         val c: Effect<String> = {
+            delay(250)
+            "baz".success()
+         }
+         IO.parN(a, b, c).getValueUnsafe() shouldBe Tuple3("foo", "bar", "baz")
+      }
+
+      test("parN(3) should short circuit").config(timeout = 500.milliseconds) {
+         val a: Effect<String> = {
+            delay(100)
+            error("boom")
+         }
+         val b: Effect<String> = {
+            delay(1000)
+            "bar".success()
+         }
+         val c: Effect<String> = {
+            delay(1000)
+            "baz".success()
+         }
+         IO.parN(a, b, c).getErrorUnsafe().message shouldBe "boom"
+      }
+
+      test("parN(4) should run in parallel").config(timeout = 350.milliseconds) {
+         val a: Effect<String> = {
+            delay(250)
+            "foo".success()
+         }
+         val b: Effect<String> = {
+            delay(250)
+            "bar".success()
+         }
+         val c: Effect<String> = {
+            delay(250)
+            "baz".success()
+         }
+         val d: Effect<String> = {
+            delay(250)
+            "woo".success()
+         }
+         IO.parN(a, b, c, d).getValueUnsafe() shouldBe Tuple4("foo", "bar", "baz", "woo")
+      }
+
+      test("parN(4) should short circuit").config(timeout = 500.milliseconds) {
+         val a: Effect<String> = {
+            delay(100)
+            error("boom")
+         }
+         val b: Effect<String> = {
+            delay(1000)
+            "bar".success()
+         }
+         val c: Effect<String> = {
+            delay(1000)
+            "baz".success()
+         }
+         val d: Effect<String> = {
+            delay(1000)
+            "woo".success()
+         }
+         IO.parN(a, b, c, d).getErrorUnsafe().message shouldBe "boom"
+      }
 
       test("IO.par should run in parallel with suspend").config(timeout = 350.milliseconds) {
          val a = IO {
