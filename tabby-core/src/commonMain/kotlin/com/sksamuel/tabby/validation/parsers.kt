b4e44ced21eae2b980059397e3379cd960356ab2
sksamuel <sam@sksamuel.com>
2021-06-10 13:04:06 -0500

    Added more parsers

--- tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/validation/parsers.kt
+++ tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/validation/parsers.kt
@@ -1,9 +1,26 @@
 package com.sksamuel.tabby.validation
 
+/**
+ * Validates any object is not null.
+ */
 fun <E, A> Validated.Companion.notNull(input: A?, ifNull: () -> E): Validated<E, A> {
    return input?.valid() ?: ifNull().invalid()
 }
 
-fun <E> Validated.Companion.notBlank(input: String?, ifNull: () -> E): Validated<E, String> {
-   return if (input.isNullOrBlank()) ifNull().invalid() else input.valid()
+/**
+ * Validates that string input is not null and not blank.
+ */
+fun <E> Validated.Companion.notNullOrBlank(input: String?, ifNullOrBlank: () -> E): Validated<E, String> {
+   return if (input.isNullOrBlank()) ifNullOrBlank().invalid() else input.valid()
+}
+
+/**
+ * Validates that string input is not blank, but null is permitted.
+ */
+fun <E> Validated.Companion.notBlank(input: String?, ifBlank: () -> E): Validated<E, String?> {
+   return when {
+      input == null -> Validated(null)
+      input.isBlank() -> ifBlank().invalid()
+      else -> input.valid()
+   }
 }
