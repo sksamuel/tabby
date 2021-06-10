545b449a2be49f9e6d3f9ffc6fa093cffc171454
sksamuel <sam@sksamuel.com>
2021-06-10 17:47:18 -0500

    Updated parsers

--- tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/validation/parsers.kt
+++ tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/validation/parsers.kt
@@ -1,7 +1,71 @@
 package com.sksamuel.tabby.validation
 
 fun interface Parser<I, A, E> {
-   companion object {}
+
+   companion object {
+
+      /**
+       * Creates a String -> String [Parser] that trims the input.
+       */
+      fun <E> trimmed(): Parser<String, String, E> = parser { it.trim().valid() }
+
+      /**
+       * Creates a String -> String [Parser] that rejects blank or null inputs.
+       */
+      fun <E> notNullOrBlank(ifNullOrBlank: () -> E): Parser<String?, String, E> {
+         return Parser { input ->
+            when {
+               input.isNullOrBlank() -> ifNullOrBlank().invalid()
+               else -> input.valid()
+            }
+         }
+      }
+
+      /**
+       * Creates a new [Parser] from String -> Int.
+       */
+      fun <E> int(ifError: (String) -> E): Parser<String, Int, E> = parser {
+         when (val int = it.toIntOrNull()) {
+            null -> ifError(it).invalid()
+            else -> int.valid()
+         }
+      }
+
+      /**
+       * Creates a new [Parser] of String -> String that requires the string to be non blank.
+       */
+      fun <E> notBlank(ifBlank: (String) -> E): Parser<String, String, E> = parser {
+         when {
+            it.isBlank() -> ifBlank(it).invalid()
+            else -> it.valid()
+         }
+      }
+
+      /**
+       * Creates a new [Parser] of String -> String that requires the string to have a min length.
+       */
+      fun <E> minlen(len: Int, ifError: (String) -> E): Parser<String, String, E> = parser {
+         when {
+            it.length < len -> ifError(it).invalid()
+            else -> it.valid()
+         }
+      }
+
+      /**
+       * Creates a new [Parser] that parses a Double from a String.
+       */
+      fun <E> double(ifError: (String) -> E): Parser<String, Double, E> = parser {
+         when (val int = it.toDoubleOrNull()) {
+            null -> ifError(it).invalid()
+            else -> int.valid()
+         }
+      }
+
+      /**
+       * Creates a new [Parser] that rejects nulls, from the provided non-nullable validating function.
+       */
+      fun <I, A, E> nullable(f: (I) -> Validated<E, A>): Parser<I?, A?, E> = parser(f).nullable()
+   }
 
    fun parse(input: I): Validated<E, A>
 }
@@ -10,7 +74,6 @@ fun interface Parser<I, A, E> {
  * Returns a [Parser] backed by the given function.
  */
 fun <I, A, E> parser(fn: (I) -> Validated<E, A>) = Parser<I, A, E> { input -> fn(input) }
-
 
 /**
  * Wraps an existing parser to accept a collection of inputs of the underlying input type I,
@@ -22,20 +85,10 @@ fun <I, A, E> Parser<I, A, E>.repeated(): Parser<List<I>, List<A>, E> {
    }
 }
 
-/**
- * Returns a new [Parser] to reject blank or null string inputs.
- */
-fun <E> Parser.Companion.notNullOrBlank(ifNullOrBlank: () -> E): Parser<String?, String, E> {
-   return Parser { input ->
-      when {
-         input.isNullOrBlank() -> ifNullOrBlank().invalid()
-         else -> input.valid()
-      }
-   }
-}
+
 
 /**
- * Modifies an existing string parser to reject blank string inputs.
+ * Modifies an existing String -> String [Parser] to reject blank string inputs.
  */
 fun <A, E> Parser<String, A, E>.notBlank(ifBlank: () -> E): Parser<String, A, E> {
    return Parser { input ->
@@ -47,7 +100,7 @@ fun <A, E> Parser<String, A, E>.notBlank(ifBlank: () -> E): Parser<String, A, E>
 }
 
 /**
- * Returns a new [Parser] from the given parser that rejects null inputs.
+ * Modifies an existing [Parser] to rejects null inputs.
  */
 fun <I, A, E> Parser<I, A, E>.notNull(ifNull: () -> E): Parser<I?, A, E> {
    return Parser { input ->
@@ -59,7 +112,7 @@ fun <I, A, E> Parser<I, A, E>.notNull(ifNull: () -> E): Parser<I?, A, E> {
 }
 
 /**
- * Returns a new [Parser] from the given parser that accepts null inputs.
+ * Modifies an existing [Parser] from the given parser that accepts null inputs.
  */
 fun <I, A, E> Parser<I, A, E>.nullable(): Parser<I?, A?, E> {
    return Parser { input ->
@@ -71,9 +124,7 @@ fun <I, A, E> Parser<I, A, E>.nullable(): Parser<I?, A?, E> {
  * Returns a [Parser] that modifies an existing parser by trimming string input prior to
  * invoking the parser.
  */
-fun <A, E> Parser<String, A, E>.trim(): Parser<String, A, E> = parser { input ->
-   this@trim.parse(input.trim())
-}
+fun <A, E> Parser<String, A, E>.trim(): Parser<String, A, E> = this.contramap { it.trim() }
 
 /**
  * Returns a [Parser] that maps a result A into a result B.
@@ -89,65 +140,22 @@ fun <I, A, B, E> Parser<I, A?, E>.mapIfNotNull(f: (A) -> B): Parser<I, B?, E> = 
 }
 
 /**
- * Creates a new [Parser] from the given non-nullable validating function that accepts null.
- */
-fun <I, A, E> Parser.Companion.nullable(f: (I) -> Validated<E, A>): Parser<I?, A?, E> = parser(f).nullable()
-
-/**
- * Creates a String parser that returns a trimmed string.
- */
-fun <E> Parser.Companion.trimmed(): Parser<String, String, E> = parser { it.trim().valid() }
-
-/**
  * Returns a [Parser] that adds additional validation to the result of a previous parser.
  */
 fun <I, A, B, E> Parser<I, A, E>.validate(f: (A) -> Validated<E, B>): Parser<I, B, E> =
    parser { input -> this@validate.parse(input).flatMap { f(it) } }
 
 /**
- * Returns a [Parser] that parses an Int from a String.
+ * Returns a method reference from I to Validated<E,A> as a [Parser].
  */
-fun <E> Parser.Companion.int(ifError: (String) -> E): Parser<String, Int, E> = parser {
-   when (val int = it.toIntOrNull()) {
-      null -> ifError(it).invalid()
-      else -> int.valid()
-   }
-}
-
 fun <I, A, E> (kotlin.reflect.KFunction1<I, Validated<E, A>>).asParser(): Parser<I, A, E> {
    val self = this@asParser
    return parser { self.invoke(it) }
 }
 
 /**
- * Returns a [Parser] from a string that requires the string to be not-blank.
+ * Creates a new [Parser] from J -> A by mapping the input to I and then feeding into
+ * the receiver parser.
  */
-fun <E> Parser.Companion.notBlank(ifBlank: (String) -> E): Parser<String, String, E> = parser {
-   when {
-      it.isBlank() -> ifBlank(it).invalid()
-      else -> it.valid()
-   }
-}
-
-/**
- * Returns a [Parser] from a string that requires the string to have a min length.
- */
-fun <E> Parser.Companion.minlen(len: Int, ifError: (String) -> E): Parser<String, String, E> = parser {
-   when {
-      it.length < len -> ifError(it).invalid()
-      else -> it.valid()
-   }
-}
-
 fun <I, J, A, E> Parser<I, A, E>.contramap(f: (J) -> I): Parser<J, A, E> =
    parser { this@contramap.parse(f(it)) }
-
-/**
- * Returns a [Parser] that parses a Double from a String.
- */
-fun <E> Parser.Companion.double(ifError: (String) -> E): Parser<String, Double, E> = parser {
-   when (val int = it.toDoubleOrNull()) {
-      null -> ifError(it).invalid()
-      else -> int.valid()
-   }
-}
