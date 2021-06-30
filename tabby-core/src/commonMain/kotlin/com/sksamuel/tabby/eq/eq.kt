b525c69c56952ad6f291863ccceaf23dc2c1e32b
sksamuel <sam@sksamuel.com>
2021-06-30 14:10:20 -0500

    Added any and none to the Eq typeclass

--- tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/eq/eq.kt
+++ tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/eq/eq.kt
@@ -3,9 +3,24 @@ package com.sksamuel.tabby.eq
 /**
  * Typeclass for equality.
  */
-fun interface Eq<A> {
-   fun eq(a1: A, a2: A): Boolean
-   fun neq(a1: A, a2: A): Boolean = !eq(a1, a2)
+fun interface Eq<T> {
+
+   fun eq(t1: T, t2: T): Boolean
+   fun neq(t1: T, t2: T): Boolean = !eq(t1, t2)
+
+   /**
+    * Returns true if the input [t] matches any of the elements of the given list [ts] when
+    * comparing using [eq] of this typeclass.
+    *
+    * @param t the needle to look for
+    * @param ts the haystack for search in
+    *
+    * @return true if ts contains t using eq of this typeclass.
+    *
+    */
+   fun any(t: T, ts: List<T>) = ts.any { eq(it, t) }
+
+   fun none(t: T, ts: List<T>) = ts.none { eq(it, t) }
 }
 
 fun <A> List<A>.any(a: A, eq: Eq<A>): Boolean = this.any { eq.eq(it, a) }
