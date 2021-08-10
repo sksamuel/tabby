2790a4d25e0eb24e84bc7b884fdc893c73bd903d
sksamuel <sam@sksamuel.com>
2021-08-10 10:03:41 -0500

    Added String.Failure bind method

--- tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/results/bind.kt
+++ tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/results/bind.kt
@@ -3,3 +3,5 @@ package com.sksamuel.tabby.results
 fun <T> T.success() = Result.success(this)
 
 fun Throwable.failure() = Result.failure<Nothing>(this)
+
+fun String.failure() = Exception(this).failure()
