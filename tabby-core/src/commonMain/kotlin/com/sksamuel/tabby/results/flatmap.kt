c83fb460f2fe42b8711fa9e6757fa961d061dd18
sksamuel <sam@sksamuel.com>
2021-08-10 09:24:02 -0500

    Added result.collect

--- tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/results/flatmap.kt
+++ tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/results/flatmap.kt
@@ -1,4 +1,5 @@
 package com.sksamuel.tabby.results
 
-fun <A, B> Result<A>.flatMap(other: (A) -> Result<B>): Result<B> = fold({ other(it) }, { Result.failure(it) })
+fun <A, B> Result<A>.flatMap(other: (A) -> Result<B>): Result<B> =
+   fold({ other(it) }, { Result.failure(it) })
 
