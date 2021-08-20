fca3ee4c0b31bc7e9f9865e69be7d639a815367c
sksamuel <sam@sksamuel.com>
2021-08-20 12:49:10 -0500

    added if null map methods

--- tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/results/flatmap.kt
+++
@@ -1,5 +0,0 @@
-package com.sksamuel.tabby.results
-
-inline fun <A, B> Result<A>.flatMap(other: (A) -> Result<B>): Result<B> =
-   fold({ other(it) }, { Result.failure(it) })
-
---
+++ tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/results/map.kt
@@ -0,0 +1,11 @@
+package com.sksamuel.tabby.results
+
+inline fun <A, B> Result<A?>.mapIfNotNull(fn: (A) -> B): Result<B?> =
+   map { if (it == null) null else fn(it) }
+
+inline fun <A, B> Result<A?>.flatMapIfNotNull(fn: (A) -> Result<B>): Result<B?> =
+   flatMap { if (it == null) null.success() else fn(it) }
+
+inline fun <A, B> Result<A>.flatMap(fn: (A) -> Result<B>): Result<B> =
+   fold({ fn(it) }, { Result.failure(it) })
+
