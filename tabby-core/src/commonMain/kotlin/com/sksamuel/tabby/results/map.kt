3120bc6fb574baa4e4a57e2caf335bf5cdb0bac6
sksamuel <sam@sksamuel.com>
2021-08-20 13:11:08 -0500

    Added mapNull / flatMapNull

--- tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/results/map.kt
+++ tabby-core/src/commonMain/kotlin/com/sksamuel/tabby/results/map.kt
@@ -1,5 +1,8 @@
 package com.sksamuel.tabby.results
 
+inline fun <A> Result<A?>.mapNull(fn: () -> A): Result<A> =
+   map { it ?: fn() }
+
 inline fun <A, B> Result<A?>.mapIfNotNull(fn: (A) -> B): Result<B?> =
    map { if (it == null) null else fn(it) }
 
@@ -8,4 +11,7 @@ inline fun <A, B> Result<A?>.flatMapIfNotNull(fn: (A) -> Result<B>): Result<B?> 
 
 inline fun <A, B> Result<A>.flatMap(fn: (A) -> Result<B>): Result<B> =
    fold({ fn(it) }, { Result.failure(it) })
+
+inline fun <A> Result<A?>.flatMapNull(fn: () -> Result<A>): Result<A> =
+   flatMap { it?.success() ?: fn() }
 
