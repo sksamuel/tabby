e6e2395a26ce6e5594095408344484fcfcb66e83
sksamuel <sam@sksamuel.com>
2021-12-19 19:25:26 -0600

    Added foldNull

--- tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/nulls/fold.kt
+++ tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/nulls/fold.kt
@@ -1,5 +1,10 @@
 package com.sksamuel.tabby.nulls
 
+@Deprecated("Use foldNull to avoid import clashes with library fold methods")
 inline fun <A, B> A?.fold(ifNull: () -> B, ifNotNull: (A) -> B): B {
+   return if (this == null) ifNull() else ifNotNull(this)
+}
+
+inline fun <A, B> A?.foldNull(ifNull: () -> B, ifNotNull: (A) -> B): B {
    return if (this == null) ifNull() else ifNotNull(this)
 }
