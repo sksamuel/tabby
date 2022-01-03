390b4c154ceab370d62b4747cbbd813094b65fba
sksamuel <sam@sksamuel.com>
2022-01-03 13:09:23 -0600

    Added firstOrNull for result list

--- tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/lists.kt
+++ tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/lists.kt
@@ -1,3 +1,7 @@
 package com.sksamuel.tabby.results
 
 fun <A, B> Result<List<A>>.mapList(f: (A) -> B): Result<List<B>> = map { it.map(f) }
+
+fun <A> Result<List<A>>.firstOrNull(f: (A) -> Boolean): Result<A?> = map { it.firstOrNull(f) }
+
+fun <A> Result<List<A>>.firstOrNull(): Result<A?> = map { it.firstOrNull() }
