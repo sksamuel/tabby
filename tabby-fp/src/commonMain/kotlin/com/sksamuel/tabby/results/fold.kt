571f2b1c0309f0ca41688429e22629bcc4d76ae4
sksamuel <sam@sksamuel.com>
2022-01-30 11:41:57 -0600

    Make foldNull inline

--- tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/fold.kt
+++ tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/fold.kt
@@ -1,5 +1,5 @@
 package com.sksamuel.tabby.results
 
-fun <T, U> Result<T?>.foldNull(ifNull: () -> U, ifNotNull: (T) -> U, ifError: (Throwable) -> U) {
+inline fun <T, U> Result<T?>.foldNull(ifNull: () -> U, ifNotNull: (T) -> U, ifError: (Throwable) -> U) {
    this.fold({ if (it == null) ifNull() else ifNotNull(it) }, ifError)
 }
