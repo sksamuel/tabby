d7927568815f8f93cce7f290f1bb269267da60eb
sksamuel <sam@sksamuel.com>
2022-04-21 09:12:03 -0500

    Bumped kotlin version

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,10 +1,10 @@
 object Libs {
 
-   const val kotlinVersion = "1.6.0"
+   const val kotlinVersion = "1.6.21"
    const val dokkaVersion = "0.10.1"
 
    object Kotest {
-      private const val version = "5.0.1"
+      private const val version = "5.2.3"
       const val shared = "io.kotest:kotest-assertions-shared:$version"
       const val assertions = "io.kotest:kotest-assertions-core:$version"
       const val junit5 = "io.kotest:kotest-runner-junit5:$version"
@@ -18,9 +18,7 @@ object Libs {
    }
 
    object Coroutines {
-      private const val version = "1.6.0-RC"
+      private const val version = "1.6.1"
       const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
-      const val coreJs = "org.jetbrains.kotlinx:kotlinx-coroutines-core-js:$version"
-      const val coreJvm = "org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:$version"
    }
 }
