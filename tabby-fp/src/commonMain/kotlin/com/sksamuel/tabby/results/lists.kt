d83e441e9da03ed8aed27d43eda6765b0fbf2021
sksamuel <sam@sksamuel.com>
2022-05-23 20:07:20 -0500

    mapList on result should be inline

--- tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/lists.kt
+++ tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/lists.kt
@@ -1,6 +1,6 @@
 package com.sksamuel.tabby.results
 
-fun <A, B> Result<List<A>>.mapList(f: (A) -> B): Result<List<B>> = map { it.map(f) }
+inline fun <A, B> Result<List<A>>.mapList(f: (A) -> B): Result<List<B>> = map { it.map(f) }
 
 fun <A> Result<List<A>>.firstOrNull(f: (A) -> Boolean): Result<A?> = map { it.firstOrNull(f) }
 
