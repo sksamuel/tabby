42d04e6de1092d2300f2604346ad2c85cb6d3442
Jonathan Lobaugh <jobelobes@users.noreply.github.com>
2022-07-15 16:01:35 -0400

    Extending failIf and failIfNot (#5)
    * extending failIf to take in exceptions

--- tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/results.kt
+++ tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/results.kt
@@ -7,27 +7,43 @@ fun <A> Result<A>.orElse(other: Result<A>): Result<A> = if (this.isFailure) othe
 
 inline fun <A> Result<A>.orElse(f: () -> Result<A>): Result<A> = if (this.isFailure) f() else this
 
-fun <A> Result<A>.failIf(p: (A) -> Boolean) = failIf(p, "failure")
+/**
+ * If this [Result] is a success, invokes the given predicate [p]. If the predicate returns true,
+ * then a failed Result is returned, otherwise this is returned.
+ */
+fun <A> Result<A>.failIf(p: (A) -> Boolean) = failIf(p, RuntimeException("failure"))
 
 /**
  * If this [Result] is a success, invokes the given predicate [p]. If the predicate returns true,
  * then a failed Result is returned, otherwise this is returned.
  */
-fun <A> Result<A>.failIf(p: (A) -> Boolean, message: String) =
-   flatMap { if (p(it)) RuntimeException(message).failure() else it.success() }
+fun <A> Result<A>.failIf(p: (A) -> Boolean, message: String) = failIf(p, RuntimeException(message))
+
+/**
+ * If this [Result] is a success, invokes the given predicate [p]. If the predicate returns true,
+ * then a failed Result is returned, otherwise this is returned.
+ */
+fun <A> Result<A>.failIf(p: (A) -> Boolean, exception: Exception) =
+   flatMap { if (p(it)) exception.failure() else it.success() }
 
 /**
  * If this [Result] is a success, invokes the given predicate [p]. If the predicate returns false,
  * then a failed Result is returned, otherwise this is returned.
  */
-fun <A> Result<A>.failIfNot(p: (A) -> Boolean) = failIfNot(p, "failure")
+fun <A> Result<A>.failIfNot(p: (A) -> Boolean) = failIfNot(p, RuntimeException("failure"))
 
 /**
  * If this [Result] is a success, invokes the given predicate [p]. If the predicate returns false,
  * then a failed Result is returned, otherwise this is returned.
  */
-fun <A> Result<A>.failIfNot(p: (A) -> Boolean, message: String) =
-   flatMap { if (!p(it)) RuntimeException(message).failure() else it.success() }
+fun <A> Result<A>.failIfNot(p: (A) -> Boolean, message: String) = failIfNot(p, RuntimeException(message))
+
+/**
+ * If this [Result] is a success, invokes the given predicate [p]. If the predicate returns false,
+ * then a failed Result is returned, otherwise this is returned.
+ */
+fun <A> Result<A>.failIfNot(p: (A) -> Boolean, exception: Exception) =
+   flatMap { if (!p(it)) exception.failure() else it.success() }
 
 /**
  * Returns a successful [Result] which contains Unit.
