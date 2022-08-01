2a9f1926e3ab06d5265a91cb59258b81665d2e13
sksamuel <sam@sksamuel.com>
2022-07-31 20:49:59 -0500

    Added mapElements

--- tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/lists.kt
+++ tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/lists.kt
@@ -1,6 +1,12 @@
 package com.sksamuel.tabby.results
 
-inline fun <A, B> Result<List<A>>.mapList(f: (A) -> B): Result<List<B>> = map { it.map(f) }
+@Deprecated("use mapElements")
+inline fun <A, B> Result<List<A>>.mapList(f: (A) -> B): Result<List<B>> = mapElements(f)
+
+/**
+ * Maps over each element of this list, if the [Result] is a success, otherwise returns this.
+ */
+inline fun <A, B> Result<List<A>>.mapElements(f: (A) -> B): Result<List<B>> = map { it.map(f) }
 
 fun <A> Result<List<A>>.firstOrNull(f: (A) -> Boolean): Result<A?> = map { it.firstOrNull(f) }
 
