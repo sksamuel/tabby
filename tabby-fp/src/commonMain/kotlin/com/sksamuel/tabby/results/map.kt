c655ed1fa116798f4d1c8c06a40616bb0ab2021c
sksamuel <sam@sksamuel.com>
2022-08-09 21:18:25 -0500

    Added mapIfNull

--- tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/map.kt
+++ tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/map.kt
@@ -14,13 +14,19 @@ inline fun <A> Result<A?>.withDefault(f: () -> A): Result<A> = replaceNull(f)
 inline fun <A, B> Result<A?>.mapIfNotNull(fn: (A) -> B): Result<B?> =
    map { if (it == null) null else fn(it) }
 
+inline fun <A> Result<A?>.mapIfNull(fn: () -> A): Result<A> =
+   map { it ?: fn() }
+
 inline fun <A, B> Result<A?>.flatMapIfNotNull(fn: (A) -> Result<B>): Result<B?> =
    flatMap { if (it == null) null.success() else fn(it) }
 
 inline fun <A, B> Result<A>.flatMap(fn: (A) -> Result<B>): Result<B> =
    fold({ fn(it) }, { Result.failure(it) })
 
-inline fun <A> Result<A?>.flatMapNull(fn: () -> Result<A>): Result<A> =
+@Deprecated("use flatMapIfNull", ReplaceWith("flatMapIfNull(fn)"))
+inline fun <A> Result<A?>.flatMapNull(fn: () -> Result<A>): Result<A> = flatMapIfNull(fn)
+
+inline fun <A> Result<A?>.flatMapIfNull(fn: () -> Result<A>): Result<A> =
    flatMap { it?.success() ?: fn() }
 
 inline fun <A> Result<A>.mapError(f: (Throwable) -> Throwable): Result<A> =
