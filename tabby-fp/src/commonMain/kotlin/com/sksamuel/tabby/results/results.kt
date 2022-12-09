61f197a8354abd6eba120266d02a268f56b56df9
sksamuel <sam@sksamuel.com>
2022-12-08 21:51:16 -0600

    Added failIfFalse

--- tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/results.kt
+++ tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/results.kt
@@ -94,6 +94,9 @@ fun <A> Result<A?>.failIfNotNull(message: String): Result<A> = failIfNotNull { E
 inline fun <A> Result<A?>.failIfNotNull(fn: () -> Exception): Result<A> =
    flatMap { it?.success() ?: Result.failure(fn()) }
 
+inline fun Result<Boolean>.failIfFalse(fn: () -> Exception): Result<Unit> =
+   flatMap { if (it) Result.unit() else Result.failure(fn()) }
+
 /**
  * Returns a successful [Result] which contains Unit.
  */
