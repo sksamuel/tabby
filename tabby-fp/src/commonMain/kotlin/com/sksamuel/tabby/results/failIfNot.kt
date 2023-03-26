5762ca5d28d537522086c86079f896a978f6fb78
sksamuel <sam@sksamuel.com>
2023-03-26 14:18:28 -0500

    Update failIfNot

---
+++ tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/failIfNot.kt
@@ -0,0 +1,38 @@
+package com.sksamuel.tabby.results
+
+/**
+ * If this [Result] is a success, invokes the given predicate [p]. If the predicate returns false,
+ * then a failed Result is returned, otherwise this is returned.
+ */
+inline fun <A> Result<A>.failIfNot(p: (A) -> Boolean) =
+   failIfNot({ RuntimeException("failure") }, p)
+
+inline fun <A> Result<A>.failIfNot(message: String, p: (A) -> Boolean) =
+   failIfNot({ RuntimeException(message) }, p)
+
+inline fun <A> Result<A>.failIfNot(exceptionFn: (A) -> Exception, p: (A) -> Boolean) =
+   flatMap { if (!p(it)) exceptionFn(it).failure() else it.success() }
+
+/**
+ * If this [Result] is a success, invokes the given predicate [p]. If the predicate returns false,
+ * then a failed Result is returned, otherwise this is returned.
+ */
+@Deprecated("use failIfNot(message, p)")
+inline fun <A> Result<A>.failIfNot(p: (A) -> Boolean, message: String) =
+   failIfNot(p, RuntimeException(message))
+
+/**
+ * If this [Result] is a success, invokes the given predicate [p]. If the predicate returns false,
+ * then a failed Result is returned, otherwise this is returned.
+ */
+@Deprecated("use failIfNot(exceptionFn, p)")
+inline fun <A> Result<A>.failIfNot(p: (A) -> Boolean, f: (A) -> Exception) =
+   flatMap { if (!p(it)) f(it).failure() else it.success() }
+
+/**
+ * If this [Result] is a success, invokes the given predicate [p]. If the predicate returns false,
+ * then a failed Result is returned, otherwise this is returned.
+ */
+@Deprecated("use failIfNot(exceptionFn, p)")
+inline fun <A> Result<A>.failIfNot(p: (A) -> Boolean, exception: Exception) =
+   flatMap { if (!p(it)) exception.failure() else it.success() }
--- tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/results.kt
+++ tabby-fp/src/commonMain/kotlin/com/sksamuel/tabby/results/results.kt
@@ -9,33 +9,6 @@ fun <A> Result<A>.orElse(other: Result<A>): Result<A> =
 inline fun <A> Result<A>.orElse(f: () -> Result<A>): Result<A> =
    if (this.isFailure) f() else this
 
-
-/**
- * If this [Result] is a success, invokes the given predicate [p]. If the predicate returns false,
- * then a failed Result is returned, otherwise this is returned.
- */
-inline fun <A> Result<A>.failIfNot(p: (A) -> Boolean) = failIfNot(p, RuntimeException("failure"))
-
-/**
- * If this [Result] is a success, invokes the given predicate [p]. If the predicate returns false,
- * then a failed Result is returned, otherwise this is returned.
- */
-inline fun <A> Result<A>.failIfNot(p: (A) -> Boolean, message: String) = failIfNot(p, RuntimeException(message))
-
-/**
- * If this [Result] is a success, invokes the given predicate [p]. If the predicate returns false,
- * then a failed Result is returned, otherwise this is returned.
- */
-inline fun <A> Result<A>.failIfNot(p: (A) -> Boolean, f: (A) -> Exception) =
-   flatMap { if (!p(it)) f(it).failure() else it.success() }
-
-/**
- * If this [Result] is a success, invokes the given predicate [p]. If the predicate returns false,
- * then a failed Result is returned, otherwise this is returned.
- */
-inline fun <A> Result<A>.failIfNot(p: (A) -> Boolean, exception: Exception) =
-   flatMap { if (!p(it)) exception.failure() else it.success() }
-
 /**
  * If this [Result] is a success containing null, returns a failure with the given message.
  * Otherwise, returns the input.
