f6f89ae37bbb117982bc8bfef5d2f440f64dff34
sksamuel <sam@sksamuel.com>
2023-03-30 07:24:31 -0500

    Added 6 arity mapN

--- tabby-fp/src/main/kotlin/com/sksamuel/tabby/results/mapn.kt
+++ tabby-fp/src/main/kotlin/com/sksamuel/tabby/results/mapn.kt
@@ -1,5 +1,9 @@
 package com.sksamuel.tabby.results
 
+/**
+ * If all inputs are successes, then executes the function [fn], otherwise returns
+ * the first failed result.
+ */
 inline fun <A, B, R> Result.Companion.mapN(
    a: Result<A>,
    b: Result<B>,
@@ -10,6 +14,10 @@ inline fun <A, B, R> Result.Companion.mapN(
    return fn(a.getOrThrow(), b.getOrThrow()).success()
 }
 
+/**
+ * If all inputs are successes, then executes the function [fn], otherwise returns
+ * the first failed result.
+ */
 inline fun <A, B, C, R> Result.Companion.mapN(
    a: Result<A>,
    b: Result<B>,
@@ -22,6 +30,10 @@ inline fun <A, B, C, R> Result.Companion.mapN(
    return fn(a.getOrThrow(), b.getOrThrow(), c.getOrThrow()).success()
 }
 
+/**
+ * If all inputs are successes, then executes the function [fn], otherwise returns
+ * the first failed result.
+ */
 inline fun <A, B, C, D, R> Result.Companion.mapN(
    a: Result<A>,
    b: Result<B>,
@@ -36,6 +48,10 @@ inline fun <A, B, C, D, R> Result.Companion.mapN(
    return fn(a.getOrThrow(), b.getOrThrow(), c.getOrThrow(), d.getOrThrow()).success()
 }
 
+/**
+ * If all inputs are successes, then executes the function [fn], otherwise returns
+ * the first failed result.
+ */
 inline fun <A, B, C, D, E, R> Result.Companion.mapN(
    a: Result<A>,
    b: Result<B>,
@@ -50,4 +66,26 @@ inline fun <A, B, C, D, E, R> Result.Companion.mapN(
    if (d.isFailure) return c as Result<R>
    if (e.isFailure) return c as Result<R>
    return fn(a.getOrThrow(), b.getOrThrow(), c.getOrThrow(), d.getOrThrow(), e.getOrThrow()).success()
+}
+
+/**
+ * If all inputs are successes, then executes the function [fn], otherwise returns
+ * the first failed result.
+ */
+inline fun <A, B, C, D, E, F, R> Result.Companion.mapN(
+   a: Result<A>,
+   b: Result<B>,
+   c: Result<C>,
+   d: Result<D>,
+   e: Result<E>,
+   f: Result<F>,
+   fn: (A, B, C, D, E, F) -> R,
+): Result<R> {
+   if (a.isFailure) return a as Result<R>
+   if (b.isFailure) return b as Result<R>
+   if (c.isFailure) return c as Result<R>
+   if (d.isFailure) return c as Result<R>
+   if (e.isFailure) return c as Result<R>
+   if (f.isFailure) return c as Result<R>
+   return fn(a.getOrThrow(), b.getOrThrow(), c.getOrThrow(), d.getOrThrow(), e.getOrThrow(), f.getOrThrow()).success()
 }
