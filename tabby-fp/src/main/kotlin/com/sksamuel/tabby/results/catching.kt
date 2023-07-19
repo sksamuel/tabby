a6ee2b894c022db245b54165ac593950938c20df
Fredrik Friis <fre.and.fri@gmail.com>
2023-07-19 14:22:45 -0400

    enhance the safety of catching (#9)
    enhance the safety of catching by rethrowing things that should never be caught (such as Errors like OutOfMemory, CancellationException etc)

--- tabby-fp/src/main/kotlin/com/sksamuel/tabby/results/catching.kt
+++ tabby-fp/src/main/kotlin/com/sksamuel/tabby/results/catching.kt
@@ -1,6 +1,5 @@
 package com.sksamuel.tabby.results
 
-import io.ktor.utils.io.CancellationException
 import kotlinx.coroutines.TimeoutCancellationException
 
 interface Catching {
@@ -14,7 +13,9 @@ inline fun <R> catching(f: Catching.() -> R): Result<R> = runCatching {
       is VirtualMachineError, // OutOfMemory and the like
       is ThreadDeath, // deprecated but still possibly thrown by old libraries etc
       is LinkageError, // dependency incompatibilities basically
-      is CancellationException, // should never be caught as doing so breaks coroutines
+      is kotlin.coroutines.cancellation.CancellationException, // should never be caught as doing so breaks coroutines
+      is kotlinx.coroutines.CancellationException, // should never be caught as doing so breaks coroutines
+      is java.util.concurrent.CancellationException, // should never be caught as doing so breaks coroutines
       is TimeoutCancellationException, // currently a subclass of the above but that will change
       is InterruptedException // same as above but threads
       -> {
