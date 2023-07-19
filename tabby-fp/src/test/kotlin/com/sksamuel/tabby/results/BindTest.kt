f9183eb7c3c76b6b3649de1e83c7bdc0ed544a3e
Fredrik Friis <fre.and.fri@gmail.com>
2023-07-19 12:48:30 -0400

    add tests for improved safety of catching

--- tabby-fp/src/test/kotlin/com/sksamuel/tabby/results/BindTest.kt
+++ tabby-fp/src/test/kotlin/com/sksamuel/tabby/results/BindTest.kt
@@ -1,7 +1,12 @@
 package com.sksamuel.tabby.results
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.result.shouldBeFailure
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withTimeout
 
 class BindTest : FunSpec() {
    init {
@@ -11,6 +16,34 @@ class BindTest : FunSpec() {
          catching {
             a.bind() + b.bind()
          } shouldBe "ab".success()
+      }
+      test("catching rethrows things that shouldn't be caught") {
+         shouldThrow<VirtualMachineError> {
+            catching { throw OutOfMemoryError("don't catch me") }
+         }
+         shouldThrow<ThreadDeath> {
+            catching { throw ThreadDeath() }
+         }
+         shouldThrow<LinkageError> {
+            catching { throw LinkageError("don't catch me") }
+         }
+         shouldThrow<kotlin.coroutines.cancellation.CancellationException> {
+            catching { throw kotlin.coroutines.cancellation.CancellationException("don't catch me") }
+         }
+         shouldThrow<kotlinx.coroutines.CancellationException> {
+            catching { throw kotlinx.coroutines.CancellationException("don't catch me") }
+         }
+         shouldThrow<java.util.concurrent.CancellationException> {
+            catching { throw java.util.concurrent.CancellationException("don't catch me") }
+         }
+         shouldThrow<TimeoutCancellationException> {
+            withTimeout(1L) { delay(1000L) }
+         }
+         shouldThrow<InterruptedException> {
+            catching { throw InterruptedException("don't catch me") }
+         }
+         val catchMe = RuntimeException("catch me")
+         catching { throw catchMe } shouldBeFailure catchMe
       }
    }
 }
