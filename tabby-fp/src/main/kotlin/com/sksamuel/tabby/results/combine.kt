171ea1f740af1e75972ebf9a724b9673a9cedf12
sksamuel <sam@sksamuel.com>
2023-08-01 03:23:24 -0500

    Update combine to be inline

--- tabby-fp/src/main/kotlin/com/sksamuel/tabby/results/combine.kt
+++ tabby-fp/src/main/kotlin/com/sksamuel/tabby/results/combine.kt
@@ -3,25 +3,25 @@ package com.sksamuel.tabby.results
 import com.sksamuel.tabby.Tuple4
 import com.sksamuel.tabby.Tuple5
 
-fun <A, B> Result<A>.combine(fn: (A) -> Result<B>): Result<Pair<A, B>> {
+inline fun <A, B> Result<A>.combine(fn: (A) -> Result<B>): Result<Pair<A, B>> {
    return this.flatMap { a ->
       fn(a).map { b -> Pair(a, b) }
    }
 }
 
-fun <A, B, C> Result<Pair<A, B>>.combine(f: (A, B) -> Result<C>): Result<Triple<A, B, C>> {
+inline fun <A, B, C> Result<Pair<A, B>>.combine(f: (A, B) -> Result<C>): Result<Triple<A, B, C>> {
    return this.flatMap { (a, b) ->
       f(a, b).map { c -> Triple(a, b, c) }
    }
 }
 
-fun <A, B, C, D> Result<Triple<A, B, C>>.combine(f: (A, B, C) -> Result<D>): Result<Tuple4<A, B, C, D>> {
+inline fun <A, B, C, D> Result<Triple<A, B, C>>.combine(f: (A, B, C) -> Result<D>): Result<Tuple4<A, B, C, D>> {
    return this.flatMap { (a, b, c) ->
       f(a, b, c).map { d -> Tuple4(a, b, c, d) }
    }
 }
 
-fun <A, B, C, D, E> Result<Tuple4<A, B, C, D>>.combine(f: (A, B, C, D) -> Result<E>): Result<Tuple5<A, B, C, D, E>> {
+inline fun <A, B, C, D, E> Result<Tuple4<A, B, C, D>>.combine(f: (A, B, C, D) -> Result<E>): Result<Tuple5<A, B, C, D, E>> {
    return this.flatMap { (a, b, c, d) ->
       f(a, b, c, d).map { e -> Tuple5(a, b, c, d, e) }
    }
