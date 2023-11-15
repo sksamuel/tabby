aa19682cde74a378d31d644f4b9cb3d98a800d56
Dustin Kozal <142238482+dustinkozal@users.noreply.github.com>
2023-11-15 13:39:58 -0600

    On success value based extensions (#11)
    * helper methods to reduce chained success + result condition calls
    
    * related tests
    
    * better naming

--- tabby-fp/src/test/kotlin/com/sksamuel/tabby/results/OnSuccessTest.kt
+++ tabby-fp/src/test/kotlin/com/sksamuel/tabby/results/OnSuccessTest.kt
@@ -3,6 +3,8 @@ package com.sksamuel.tabby.results
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.shouldBe
 
 class OnSuccessTest : FunSpec() {
    init {
@@ -33,6 +35,72 @@ class OnSuccessTest : FunSpec() {
          var fired = false
          Result.success(name).onSuccessIfNull { fired = true }
          fired.shouldBeTrue()
+      }
+
+      test("onSuccessIfTrue with failure") {
+         var fired = false
+         Result.failure<Boolean>(Exception("")).onSuccessIfTrue { fired = true }
+         fired.shouldBeFalse()
+      }
+
+      test("onSuccessIfTrue with true") {
+         val bool = true
+         var fired = false
+         Result.success(bool).onSuccessIfTrue { fired = true }
+         fired.shouldBeTrue()
+      }
+
+      test("onSuccessIfTrue with false") {
+         val bool = false
+         var fired = false
+         Result.success(bool).onSuccessIfTrue { fired = true }
+         fired.shouldBeFalse()
+      }
+
+      test("onSuccessIfFalse with true") {
+         val bool = true
+         var fired = false
+         Result.success(bool).onSuccessIfFalse { fired = true }
+         fired.shouldBeFalse()
+      }
+
+      test("onSuccessIfFalse with false") {
+         val bool = false
+         var fired = false
+         Result.success(bool).onSuccessIfFalse { fired = true }
+         fired.shouldBeTrue()
+      }
+
+      test("onSuccessIf with delegate") {
+         val inputParam = "test-string"
+         val delegate: (String) -> Boolean = { true }
+         var seenValue: String? = null
+         Result.success(inputParam).onSuccessIf(delegate) { seenValue = it }
+         seenValue shouldBe inputParam
+      }
+
+      test("onSuccessIfNotNull with null") {
+         val inputParam: String = "test-string"
+         val delegate: (String?) -> Boolean = { false }
+         var seenValue: String? = null
+         Result.success(inputParam).onSuccessIfNotNull(delegate) { seenValue = it }
+         seenValue.shouldBeNull()
+      }
+
+      test("onSuccessIfNotNull with not null and false") {
+         val inputParam = "test-string"
+         val delegate: (String) -> Boolean = { false }
+         var seenValue: String? = null
+         Result.success(inputParam).onSuccessIfNotNull(delegate) { seenValue = it }
+         seenValue.shouldBeNull()
+      }
+
+      test("onSuccessIfNotNull with not null and true") {
+         val inputParam = "test-string"
+         val delegate: (String) -> Boolean = { true }
+         var seenValue: String? = null
+         Result.success(inputParam).onSuccessIfNotNull(delegate) { seenValue = it }
+         seenValue shouldBe inputParam
       }
    }
 }
