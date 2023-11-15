5dbcf5a17026627b3a1fcd65ad658fa3c84747a0
Dustin Kozal <dustin.kozal@grindr.com>
2023-11-15 13:12:20 -0600

    helper methods to reduce chained success + result condition calls

--- tabby-fp/src/main/kotlin/com/sksamuel/tabby/results/onSuccess.kt
+++ tabby-fp/src/main/kotlin/com/sksamuel/tabby/results/onSuccess.kt
@@ -3,9 +3,32 @@ package com.sksamuel.tabby.results
 inline fun <A> Result<A?>.onSuccessIfNotNull(f: (A) -> Unit) =
    onSuccess { if (it != null) f(it) }
 
-inline fun <A> Result<A?>.onSuccessIfNull(f: () -> Unit): Result<A?> {
-   return this.fold({
+inline fun <A> Result<A?>.onSuccessIfNull(f: () -> Unit): Result<A?> =
+   this.fold({
       if (it == null) f()
       it.success()
    }, { it.failure() })
-}
+
+inline fun <A> Result<A>.onSuccessOnlyIf(predicate: (A) -> Boolean, f: (A) -> Unit): Result<A> =
+   this.fold({
+      if (predicate(it)) f(it)
+      it.success()
+   }, { it.failure() })
+
+inline fun Result<Boolean>.onSuccessIfTrue(f: () -> Unit): Result<Boolean> =
+   this.fold({
+      if (it) f()
+      it.success()
+   }, { it.failure() })
+
+inline fun Result<Boolean>.onSuccessIfFalse(f: () -> Unit): Result<Boolean> =
+   this.fold({
+      if (!it) f()
+      it.success()
+   }, { it.failure() })
+
+inline fun <A> Result<A?>.onSuccessOnlyIfNotNull(predicate: (A) -> Boolean, f: (A) -> Unit): Result<A?> =
+   this.fold({
+      if ((it != null) && predicate(it)) f(it)
+      it.success()
+   }, { it.failure() })
