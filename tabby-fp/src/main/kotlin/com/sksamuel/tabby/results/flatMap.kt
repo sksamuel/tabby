0fa37638712efd6b059f280f95a8a2f87c95af89
Dustin Kozal <142238482+dustinkozal@users.noreply.github.com>
2023-11-16 11:56:06 -0600

    Flatmapif with default (#12)
    * helper methods to reduce chained success + result condition calls
    
    * related tests
    
    * better naming
    
    * change to flatMapIf to provide a default and return as B

--- tabby-fp/src/main/kotlin/com/sksamuel/tabby/results/flatMap.kt
+++ tabby-fp/src/main/kotlin/com/sksamuel/tabby/results/flatMap.kt
@@ -3,8 +3,8 @@ package com.sksamuel.tabby.results
 inline fun <A, B> Result<A>.flatMap(fn: (A) -> Result<B>): Result<B> =
    fold({ fn(it) }, { Result.failure(it) })
 
-inline fun <A, B> Result<A>.flatMapIf(p: (A) -> Boolean, f: (A) -> Result<A>): Result<A> =
-   flatMap { if (p(it)) f(it) else it.success() }
+inline fun <A, B> Result<A>.flatMapIf(p: (A) -> Boolean, f: (A) -> Result<B>, default: B): Result<B> =
+   flatMap { if (p(it)) f(it) else default.success() }
 
 inline fun <A, B> Result<A>.flatMapIf(p: (A) -> Boolean, f: (A) -> Result<B>, g: (A) -> Result<B>): Result<B> =
    flatMap { if (p(it)) f(it) else g(it) }
